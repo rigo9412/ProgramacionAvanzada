50dacdc85fc7fe8d3b42317ae7bccfe7c0cedc75
JuanNeiraJaime <l17100264@nlared.tecnm.mx>
2022-08-23 10:46:11 -0600

    17100264

--- lista.js
+++ lista.js
@@ -5,4 +5,5 @@
 //18100149 Jesús Alfonso Andrade Domínguez
 //17100250 Luis Armando Martinez Rubio
 //18100165 Francisco Javier De la Rosa Gómez
+//17100264 Juan Antonio Neira Jaime
 // otro commit 
