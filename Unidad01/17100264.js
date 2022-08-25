799b76c7bbf09ffe72dcf41ad57d518e2b6b9258
JuanNeiraJaime <l17100264@nlared.tecnm.mx>
2022-08-25 10:59:26 -0600

    test3

--- Unidad01/17100264.js
+++ Unidad01/17100264.js
@@ -1,11 +1,1 @@
-
-    var n1, n2, sum, text;
-
-    n1 = document.getElementById("Primer dato: ").value;
-    n2 = document.getElementById("Segundo dato: ").value;
-
-    sum = parseFloat(n1)+parseFloat(n2);
-
-    text= sum;
-
-    document.getElementById("sumando").innerHTML = text;
+//Juan Antonio Neira Jaime
---
+++ Unidad01/suma.js
@@ -0,0 +1,6 @@
+    var n1, n2;
+
+    n1 = 10
+    n2 = 5
+
+    console.log(n1+n2)
