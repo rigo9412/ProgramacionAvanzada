cee761927695de4e0a303da86e26b1b7e5df5f6a
AlfonsoAD <alfonsad097@gmail.com>
2022-08-25 12:23:23 -0500

    18100149

--- Unidad01/18100149.js
+++ Unidad01/18100149.js
@@ -1,1 +1,5 @@
-//18100149
+let num1=5
+let num2=9
+let resultado
+
+console.log("El resultado es: " + (resultado = num1 + num2))
--- Unidad01/Suma-18100149.js
+++
@@ -1,5 +0,0 @@
-let num1=5
-let num2=9
-let resultado
-
-console.log("El resultado es: " + (resultado = num1 + num2))
