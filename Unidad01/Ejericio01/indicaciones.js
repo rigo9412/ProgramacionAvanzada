9e9230465d3addd1e0e61c230327e3c916df5c3a
rigo9412 <rigo.ramos.9412@hotmail.com>
2022-08-24 22:11:28 -0500

    Ejercios

---
+++ Unidad01/Ejericio01/indicaciones.js
@@ -0,0 +1,6 @@
+/*Elaborar un algoritmo que invierta una cadena de texto
+
+Ejemplo: abc 
+Resultado:
+cba
+*/
---
+++ Unidad01/Ejericio03/indicaciones.js
@@ -0,0 +1,11 @@
+/*
+
+Elaborar un algoritmo que reciba un arreglo y cuente los registros que son verdaderos (true)
+
+
+Ejemplo 
+[true,false,"0","1","false","true"]
+
+Resultado = 1
+
+*/
