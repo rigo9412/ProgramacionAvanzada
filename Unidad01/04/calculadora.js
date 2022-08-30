949a85532f64fda242761dbe3347acebec696f4c
L17100250 <L17100250@nlaredo.tecnm.mx>
2022-08-30 16:42:27 -0500

    Correcion ejercicio 3 y agregacion de carpeta de ejercicio 4

---
+++ Unidad01/04/calculadora.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(e)){
+    e.preventDefault();
+
+    var num1 = number(document.getElementById('num1'));
+
+});
+
+const sumar = (num1, num2) => num1 + num2
+const sumar = (num1, num2) => num1 + num2
+const sumar = (num1, num2) => num1 + num2
+const sumar = (num1, num2) => num1 + num2
