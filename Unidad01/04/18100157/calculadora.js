34b6bd237b639c64a2d26a05a50404ad920d01ab
Cesar-CCS <CesarCastro_S@outlook.com>
2022-09-04 22:05:52 -0500

    Cambios Historial

--- Unidad01/04/18100157/calculadora.js
+++ Unidad01/04/18100157/calculadora.js
@@ -5,6 +5,7 @@ const Nums = document.getElementsByName("nume");
 const Operadores=['+','-','X','/','%','^','√','1/x'];
 const TE=['Enter','='];
 const Borrar=['C','c'];
+const historial=[]
 const sumar =(numero1,numero2) => numero1 + numero2
 const multiplicacion =(numero1,numero2) => numero1 * numero2
 const exponente =(numero1,numero2) => Math.pow(numero1,numero2)
@@ -24,68 +25,76 @@ Nums.forEach(function(boton){
 }
 )
 
+function mostrar(operacion){
+    //Input1=operacion.substring()
+}
+
 function Calcular(key){
     if (!isNaN(key) || key == '.') {
         Input2.value+=key;
     }
+    
     else if(Operadores.includes(key)){
-        if (key == '1/x') {
-            console.log("Entra")
-            Input1.value=1/+" "+ Input2.value;
+        if(key=='√'){
+            Input1.value=key+" "+ Input2.value;
             Input2.value="";
-        }
-        else{
-            console.log("entra")
+        }else{
             Input1.value=Input2.value+" "+ key;
             Input2.value="";
         }
-    }
-    else if(Operadores.includes(key)){
-        Input1.value=Input2.value+" "+ key;
-        Input2.value="";
-    }
-    else if (Borrar.includes(key)) {
+    }else if (Borrar.includes(key)) {
         Input1.value="";
         Input2.value="";
     }
     else if(TE.includes(key)){
         const Aux = (Input1.value+" "+Input2.value).split(" ");
-        console.log(Aux);
         switch(Aux[1]){
             case '+':
                 sumar(Number(Aux[0]),Number(Aux[2]))
-                Input1.value=Input1.value+" "+Input2.value + '=';
+                Input1.value=Input1.value+" "+Input2.value + ' =';
                 Input2.value=sumar(Number(Aux[0]),Number(Aux[2]));
             break;
             case '-':
                 resta(Number(Aux[0]),Number(Aux[2]))
-                Input1.value=Input1.value+" "+Input2.value + '=';
+                Input1.value=Input1.value+" "+Input2.value + ' =';
                 Input2.value=resta(Number(Aux[0]),Number(Aux[2]));
             break;
             case 'X':
                 multiplicacion(Number(Aux[0]),Number(Aux[2]))
-                Input1.value=Input1.value+" "+Input2.value + '=';
+                Input1.value=Input1.value+" "+Input2.value + ' =';
                 Input2.value=multiplicacion(Number(Aux[0]),Number(Aux[2]));
             break;
             case '/':
                 dividir(Number(Aux[0]),Number(Aux[2]))
-                Input1.value=Input1.value+" "+Input2.value + '=';
+                Input1.value=Input1.value+" "+Input2.value + ' =';
                 Input2.value=dividir(Number(Aux[0]),Number(Aux[2]));
             break;
             case '^':
                 exponente(Number(Aux[0]),Number(Aux[2]))
-                Input1.value=Input1.value+" "+Input2.value + '=';
+                Input1.value=Input1.value+" "+Input2.value + ' =';
                 Input2.value=exponente(Number(Aux[0]),Number(Aux[2]));
             break;
-            case '√':
-                console.log('hola')
-                raiz(Number(Aux[0]))
-                Input1.value=Input1.value + '=';
-                Input2.value=raiz(Number(Aux[0]));
-            break;
-            /*case 'C':
-                Input1.value="";
-                Input2.value="";*/
         }
+        if(Aux[0]=='√'){
+            console.log('raiz')
+            raiz(Number(Aux[1]))
+                Input1.value=Input1.value + '=';
+                Input2.value=raiz(Number(Aux[1]));
+        }
+        historial[historial.length] = Input1.value+" "+Input2.value
+        console.log(historial)
+        mostrarHistorial();
+    }
+}
+
+function mostrarHistorial() {
+    let History = document.getElementById("History")
+    History.innerHTML = ''
+
+    for (const dato of historial) {
+        let datoHistorial = document.createElement('p');
+        datoHistorial.setAttribute('id','H_parrafo')
+        datoHistorial.innerText = dato
+        History.appendChild(datoHistorial)
     }
 }
