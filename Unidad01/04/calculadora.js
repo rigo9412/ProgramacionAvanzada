a2b1aa10ce6013cf9475bba4d29ba60389f0f79c
JordanAleman99 <alejandroaleman93@outlook.com>
2022-09-05 07:42:51 -0500

    Act

--- Unidad01/04/calculadora.js
+++ Unidad01/04/calculadora.js
@@ -1,10 +1,9 @@
 const calc = document.getElementById("body");
 const historial = document.getElementById("txt1");
 const valores = document.getElementById("txt2");
-
 const buton = document.getElementsByName('numero')
 
-const arregloOperaciones=['+','-','*','/','^'];
+const arregloOperaciones=['+','-','*','/','^','√'];
 const arregloAccion=['Enter','='];
 const borrar=['c','CE'];
 
@@ -13,6 +12,7 @@ const Resta = (num1,num2) => num1 - num2
 const Multi = (num1,num2) => num1 * num2
 const Expo = (num1,num2) => num1 ** num2
 const Division  = (num1,num2) => num1 / num2
+const Raiz = (num1) => Math.sqrt(num1);
 
 calc.addEventListener('keypress',(evento) => 
 {
@@ -25,6 +25,7 @@ buton.forEach(function(e){
     e.addEventListener('click', () => {
          
       Calcular(e.value);
+      
     })
     
 });
@@ -45,10 +46,15 @@ function Calcular(key){
 
         historial.value="";
         valores.value="";
+    
+    
+    } else if(key == "C"){
 
-    } 
-    else if(arregloAccion.includes(key)){
+        valores.value = "";
 
+    }else if(arregloAccion.includes(key)){
+
+        
         const aux = (historial.value + " " + valores.value).split(" ");
         switch(aux[1]){
 
@@ -60,29 +66,32 @@ function Calcular(key){
 
             case '-' :
                 Resta(Number(aux[0]),Number(aux[2]))
-                historial.value = historial.value + " "+ valores.value + ' =';
+                historial.value = historial.value + " "+ valores.value + ' ';
                 valores.value = Resta(Number(aux[0]),Number(aux[2]))
             break;
 
             case '^' :
                 Expo(Number(aux[0]),Number(aux[2]))
-                historial.value = historial.value + " " + valores.value + ' =';
+                historial.value = historial.value + " " + valores.value + ' ';
                 valores.value = Expo(Number(aux[0]),Number(aux[2]))
             break;
 
             case '*' :
                 Multi(Number(aux[0]),Number(aux[2]))
-                historial.value = historial.value + " " + valores.value + ' =';
+                historial.value = historial.value + " " + valores.value + ' ';
                 valores.value = Multi(Number(aux[0]),Number(aux[2]))
             break;
 
             case '/' : 
             Division(Number(aux[0]),Number(aux[2]))
-                historial.value = historial.value + " " + valores.value + ' =';
+                historial.value = historial.value + " " + valores.value + ' ';
                 valores.value = Division(Number(aux[0]),Number(aux[2]))
-            break;
+            break;   
 
         }
+
+       
+        
 
     }
 
