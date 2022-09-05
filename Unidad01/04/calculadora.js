c50748c419db216d72e7451c1d97049a2564c092
JordanAleman99 <alejandroaleman93@outlook.com>
2022-09-05 11:04:51 -0500

    145

--- Unidad01/04/calculadora.js
+++ Unidad01/04/calculadora.js
@@ -3,16 +3,19 @@ const historial = document.getElementById("txt1");
 const valores = document.getElementById("txt2");
 const buton = document.getElementsByName('numero')
 
-const arregloOperaciones=['+','-','*','/','^','√'];
+const arregloOperaciones=['+','-','*','/','^','√','%','1/x'];
 const arregloAccion=['Enter','='];
 const borrar=['c','CE'];
-
+const Cuac =['√'];
 const Sumar = (num1,num2) => num1 + num2
 const Resta = (num1,num2) => num1 - num2
 const Multi = (num1,num2) => num1 * num2
 const Expo = (num1,num2) => num1 ** num2
 const Division  = (num1,num2) => num1 / num2
 const Raiz = (num1) => Math.sqrt(num1);
+const Porcentaje = (num1,num2) => (num1 / 100) * num2
+const Dif = (num1) => 1 / num1;
+
 
 calc.addEventListener('keypress',(evento) => 
 {
@@ -52,16 +55,27 @@ function Calcular(key){
 
         valores.value = "";
 
+    }else if(key == "←"){
+        
+        valores.value=valores.value.substring(0,valores.value.length-1);
+
+    } else if(key == "+/-"){
+
+        valores.value = valores.value * -1;
+
+
     }else if(arregloAccion.includes(key)){
 
         
         const aux = (historial.value + " " + valores.value).split(" ");
+
         switch(aux[1]){
 
             case '+' :
                 Sumar(Number(aux[0]),Number(aux[2]))
                 historial.value = historial.value + " " + valores.value + ' ';
                 valores.value = Sumar(Number(aux[0]),Number(aux[2]))
+
             break;
 
             case '-' :
@@ -88,9 +102,33 @@ function Calcular(key){
                 valores.value = Division(Number(aux[0]),Number(aux[2]))
             break;   
 
+            case '%' :
+            Porcentaje(Number(aux[0]),Number(aux[2]))
+
+                historial.value = historial.value + " " + valores.value + ' ';
+                valores.value = Porcentaje(Number(aux[0]),Number(aux[2]))
+            
+            break;
+
+            case '1/x' :
+            Dif(Number(aux[0]))
+                
+                valores.value = Dif(Number(aux[0]))
+
+            break;
+                
+
         }
 
-       
+        if(aux[1] == '√'){
+            
+            var R = Raiz(Number(aux[0]))
+            valores.value = R;
+
+            historial.value = aux[1] + " " + aux[0] + "=";
+
+        }
+
         
 
     }
