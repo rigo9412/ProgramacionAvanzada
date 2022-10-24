78926ab92bcef678572dfd43af1f228be435e1c3
Cesar-CCS <CesarCastro_S@outlook.com>
2022-10-23 23:44:42 -0500

    calculadora

---
+++ Unidad03.1/18100157 calculadora/calculadora/components/button_action.js
@@ -0,0 +1,16 @@
+const ButtonAction = ({ title,value, color = 'bg-slate-400', hoverColor = 'bg-slate-600', textColor = "text-white", onClickAction }) => {
+    
+    const localClick = (e) => {
+        console.log(value);
+        onClickAction(value);
+    }
+    
+    return <div className="p-2">
+        <button
+            className={`w-20 h-20 rounded-full ${color} hover:${hoverColor} ${textColor}`}
+            onClick={localClick}>
+            <span className="text-4xl" >{title}</span>
+        </button>
+    </div>
+}
+export default ButtonAction;
